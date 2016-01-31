4469d30e75f6c26bc79fda57fd3cd65041e98e80
Bibi10 <bdjafalo@outlook.com>
2016-01-31 11:35:42 +0100

    add index.jade

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express	= require('express');
 var app		= express();
 var port	= process.env.PORT || 8080;
-var db		= require('db');
-var passport	= require('passport');
+var db		= require('./config/db');
+//var passport	= require('./config/passport');
 var flash	= require('connect-flash');
 
 var morgan		= require('morgan');
-var cookieParser 	= require(cookie-parser');
+var cookieParser 	= require('cookie-parser');
 var bodyParser		= require('body-parser');
 var session     	= require('express-session');
 
@@ -18,8 +18,14 @@ var session     	= require('express-session');
     app.set('view engine', 'jade'); // set up jade for templating
 
     // required for passport
+	require('./config/passport')(passport)
     app.use(session({ secret: 'imbibixessongdjafalo' }));
     app.use(passport.initialize());
     app.use(passport.session()); 
     app.use(flash()); 
+	// routes
+	require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+	// launch ======================================================================
+	app.listen(port);
+	console.log('The magic happens on port ' + port);
